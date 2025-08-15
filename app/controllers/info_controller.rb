2de6063457c578cbad720f1f28f032dfaccec07a
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-08-15 21:01:42 +0200

    Avances en la página home. Queda la sección de elemento aleatorio, lo demás listo.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,5 +1,9 @@
 class InfoController < ApplicationController
     def home
+        @clase_image = Clase.where("oculto IS FALSE AND image IS NOT NULL").order("RANDOM()").first.image || nil
+        @habilidad_image = nil #Habilidad.where("image IS NOT NULL").order("RANDOM()").first.image || nil
+        @item_image = Item.where("image IS NOT NULL").order("RANDOM()").first.image || nil
+        @carousel_images = Picture.order("RANDOM()").limit(10).map(&:image)
     end
     def reglas
     end
