4ecddc848d9e56dcd7fe3e703216e21ea5644b8e
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-08-30 11:32:10 +0200

    esconder categorías en liveSearch.js

--- app/javascript/liveSearch.js
+++ app/javascript/liveSearch.js
@@ -1,12 +1,19 @@
 document.addEventListener("turbo:load", () => {
   const input = document.getElementById("buscador-input");
+  
   input.addEventListener("input", () => {
     const query = input.value.toLowerCase().trim();
+
     document.querySelectorAll(".grid-container").forEach(grid => {
       grid.querySelectorAll(".carta").forEach(carta => {
         const texto = carta.innerText.toLowerCase();
         carta.style.display = texto.includes(query) ? "flex" : "none";
       });
+    });
+
+    document.querySelectorAll(".categ").forEach(categ => {
+      const cartasVisibles = categ.querySelectorAll(".carta:not([style*='display: none'])");
+      categ.style.display = cartasVisibles.length > 0 ? "block" : "none";
     });
   });
 });
